ff7d4d7e1343a924123cee9ba3dc9df5a722c40e
Fercho Posting <shaaf4ever@gmail.com>
2021-09-22 00:23:10 -0500

    Field helper with class!

--- src/FieldHelper.js
+++ src/FieldHelper.js
@@ -1,9 +1,51 @@
-export default function (name, label, type = "text", options = {}) {
-  return {
-    name,
-    label,
-    type,
+class Field {
+  constructor(name, label, type = "text") {
+    this.name = name;
+    this.label = label;
+    this.type = type;
 
-    ...options,
-  };
+    return this;
+  }
+
+  extend(binds) {
+    Object.keys(binds).forEach((key) => {
+      this[key] = binds[key];
+    });
+
+    return this;
+  }
+
+  required() {
+    if (typeof this.attrs == typeof undefined) {
+      this.attrs = {};
+    }
+
+    this.attrs.required = true;
+
+    return this;
+  }
+
+  email() {
+    this.type = "email";
+
+    return this;
+  }
+
+  tel() {
+    this.type = "tel";
+
+    return this;
+  }
+
+  date() {
+    this.type = "date";
+
+    return this;
+  }
 }
+
+function f() {
+  return new Field(...arguments);
+}
+
+export default f;
