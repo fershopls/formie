46a494f909223caf18dfadf4a687e9995d7f410b
Fercho Posting <shaaf4ever@gmail.com>
2021-09-21 23:33:56 -0500

    Better textarea

--- src/Inputs/Textarea.vue
+++ src/Inputs/Textarea.vue
@@ -1,35 +1,23 @@
 <template>
-  <div class="text-gray-400 focus-within:text-gray-500">
-    <div class="text-sm font-bold uppercase tracking-widest
-    transition-all duration-500 ease-in-out">
-      {{ label }}
-    </div>
+  <div>
     <div
-      class="mt-2
-    grid
-    border border-gray-300 rounded
-    p-2
-    overflow-hidden
-    cursor-text
-    "
+      class="border rounded border-gray-300 cursor-text flex"
       @click="focusTextarea"
     >
       <textarea
         class="resize-none box-border
-      bg-transparent
-      outline-none ring-none border-none
-      focus:outline-none focus:ring-transparent focus:border-none
-      text-gray-700"
+        p-2 w-full
+        bg-white
+        outline-none ring-none border-none
+        focus:outline-none focus:ring-transparent focus:border-none"
         v-model="currentValue"
         ref="textarea"
-        :rows="rows"
+        v-bind="field.attrs"
         @change="resize"
         @cut="resize"
         @paste="resize"
         @drop="resize"
         @input="resize"
-        @blur="$emit('blur')"
-        :placeholder="placeholder"
       ></textarea>
     </div>
   </div>
@@ -46,21 +34,16 @@ textarea {
 
 <script>
 export default {
-  props: {
-    value: String,
-    label: String,
-    placeholder: String,
-    rows: {
-      type: Number,
-      default: 1,
-    },
-  },
-  emits: ["update", "blur"],
+  props: ["field", "value", "context"],
+
+  emits: ["update"],
+
   data() {
     return {
       currentValue: this.value,
     };
   },
+
   watch: {
     value(value) {
       this.currentValue = value;
@@ -69,15 +52,18 @@ export default {
       this.$emit("update", this.currentValue);
     },
   },
+
   mounted() {
     this.resize();
   },
+
   methods: {
     resize() {
       this.$refs.textarea.style.height = `auto`;
       const scrollHeight = this.$refs.textarea.scrollHeight;
       this.$refs.textarea.style.height = `${scrollHeight}px`;
     },
+
     focusTextarea() {
       this.$refs.textarea.focus();
     },
