d6fcef5ff26a82c5b75b9e91b939eaf95a5bf1a7
Fercho Posting <shaaf4ever@gmail.com>
2021-09-22 13:17:40 -0500

    Custom group component

---
+++ Vite/GroupWithTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div
+      v-if="group.title"
+      class="text-2xl text-center px-4 uppercase font-bold tracking-wider py-4 border-t border-b"
+    >{{ group.title }}</div>
+    <div class="mt-2 grid gap-4 px-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["group"],
+};
+</script>
--- Vite/Vite.vue
+++ Vite/Vite.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-lg my-12 mx-auto border border-gray-200 rounded p-6">
+  <div class="max-w-lg my-12 mx-auto border border-gray-200 rounded">
 
     <formie
       class="grid gap-4"
@@ -15,6 +15,7 @@
 
 <script>
 import { Formie, inputs, f, group } from "../src/main.js";
+import GroupWithTitle from "./GroupWithTitle.vue";
 
 export default {
   components: { Formie },
@@ -22,48 +23,60 @@ export default {
   setup() {
     const form = [
       group([
-        f("client.name", "Cliente: Nombre").required(),
-        f("client.lastname", "Cliente: Apellidos").required(),
-      ]).class("grid gap-4 grid-cols-2"),
-
-      // Client
-      f("client.birth_date", "Cliente: Fecha de Nacimiento").date().required(),
-      f("client.country", "Cliente: País").required(),
-      f("client.city", "Cliente: Ciudad").required(),
-      f("client.email", "Cliente: Correo Electrónico").email().required(),
-      f("client.phone", "Cliente: Teléfono", "phone").tel().required(),
-
-      // Goal
-      f("antecedents", "Antecedentes", inputs.Textarea).extend({
-        attrs: { rows: 3, placeholder: "Situacion actual..." },
+        group([
+          f("client.name", "Cliente: Nombre").required(),
+          f("client.lastname", "Cliente: Apellidos").required(),
+        ]).class("grid gap-4 grid-cols-2"),
+        f("client.birth_date", "Cliente: Fecha de Nacimiento")
+          .date()
+          .required(),
+        f("client.country", "Cliente: País").required(),
+        f("client.city", "Cliente: Ciudad").required(),
+        f("client.email", "Cliente: Correo Electrónico").email().required(),
+        f("client.phone", "Cliente: Teléfono", "phone").tel().required(),
+      ]).type(GroupWithTitle, {
+        title: "Cliente",
       }),
 
-      f("type", "¿Qué protocolo vas a realizar?", inputs.Select).extend({
-        options: ["Protocolo General", "Protocolo de Salud"],
-      }),
+      group([
+        // Goal
+        f("antecedents", "Antecedentes", inputs.Textarea).extend({
+          attrs: { rows: 3, placeholder: "Situacion actual..." },
+        }),
 
-      f("goal", "¿Cuál es el objetivo?", inputs.Textarea),
+        f("type", "¿Qué protocolo vas a realizar?", inputs.Select).extend({
+          options: ["Protocolo General", "Protocolo de Salud"],
+        }),
 
-      f("kind", "¿En qué categoría encaja el objetivo?", inputs.Select).extend({
-        options: [
-          "Autoestima",
-          "Depresión y otros estados emocionales",
-          "Dinero",
-          "Pareja y relaciones",
-          "Sobrepeso",
-          "Trabajo",
-          "Adicciones",
-          "Enfermedades mentales",
-          "Otros",
-        ],
-      }),
+        f("goal", "¿Cuál es el objetivo?", inputs.Textarea),
 
-      f("details", "Detalles del Objetivo", inputs.Textarea).extend({
-        attrs: {
-          rows: 3,
-          placeholder:
-            "Introduce los detalles y definiciones necesarias para que el objetivo quede perfectamente definido. Por ejemplo, significado de palabras como óptimo, ideal, mejor, peor, …, características de la pareja ideal, del trabajo ideal, etc.",
-        },
+        f(
+          "kind",
+          "¿En qué categoría encaja el objetivo?",
+          inputs.Select
+        ).extend({
+          options: [
+            "Autoestima",
+            "Depresión y otros estados emocionales",
+            "Dinero",
+            "Pareja y relaciones",
+            "Sobrepeso",
+            "Trabajo",
+            "Adicciones",
+            "Enfermedades mentales",
+            "Otros",
+          ],
+        }),
+
+        f("details", "Detalles del Objetivo", inputs.Textarea).extend({
+          attrs: {
+            rows: 3,
+            placeholder:
+              "Introduce los detalles y definiciones necesarias para que el objetivo quede perfectamente definido. Por ejemplo, significado de palabras como óptimo, ideal, mejor, peor, …, características de la pareja ideal, del trabajo ideal, etc.",
+          },
+        }),
+      ]).type(GroupWithTitle, {
+        title: "Protocolo",
       }),
 
       // Buttons
